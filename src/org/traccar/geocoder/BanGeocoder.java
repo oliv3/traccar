c5746ec4111a5a68595ce6f2423963b1dec8bd00
Olivier Girondel <olivier@biniou.info>
2017-07-03 03:58:50 +0200

    Update copyright

--- src/org/traccar/geocoder/BanGeocoder.java
+++ src/org/traccar/geocoder/BanGeocoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2015 Anton Tananaev (anton@traccar.org)
+ * Copyright 2017 Olivier Girondel (olivier@biniou.info)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
